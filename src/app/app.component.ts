4d103891f0c1b2a6ee7e1379d7e0542a800ae53b
anwar_aqraa <anwaraqraa932@gmail.com>
2025-04-26 23:00:56 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { provideRouter, RouterModule } from '@angular/router';
-import { routes } from './app.routes';
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
